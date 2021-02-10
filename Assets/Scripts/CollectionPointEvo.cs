2f9d9f09f3cf61b9da83bbc60cb6046e9ba65943
Brian Nguyen <nguyen.brian70@gmail.com>
2021-02-09 21:53:05 -0500

    Add agents in scene

---
+++ Assets/Scripts/CollectionPointEvo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectionPointEvo : MonoBehaviour
+{
+    public PushCollectorAgentvsAgent agentToGivePoints;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        // Touched goal.
+        if (col.gameObject.CompareTag("resource"))
+        {
+            agentToGivePoints.CollectedResource();
+        }
+    }
+}
