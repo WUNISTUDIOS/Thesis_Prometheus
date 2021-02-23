20d27bbbb334343a0208c7687ef8611e76d477e3
Brian Nguyen <nguyen.brian70@gmail.com>
2021-02-23 16:25:52 -0500

    Revert "Revert "API upgrade backup""
    This reverts commit 1ea0f7a543e6f8a8a98130daedd9a848bed1448a.

---
+++ Assets/NavAgentController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavAgentController : MonoBehaviour
+{
+    NavMeshAgent myNavMeshAgent;
+    void Start()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetDestinationToMousePosition();
+        }
+    }
+
+    void SetDestinationToMousePosition()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            myNavMeshAgent.SetDestination(hit.point);
+        }
+    }
+}
