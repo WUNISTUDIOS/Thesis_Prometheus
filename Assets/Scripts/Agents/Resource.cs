708ceb28f68866bd32c8807e5d65942a50da339b
Brian Nguyen <nguyen.brian70@gmail.com>
2021-03-17 01:01:46 -0400

    Add audio example

--- Assets/Scripts/Agents/Resource.cs
+++ Assets/Scripts/Agents/Resource.cs
@@ -1,11 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Resource : MonoBehaviour
 {
+
     public bool collected = false;
     public bool held = false;
     public GameObject resourceArea;
 
+    AudioSource OnStartSound;
+
+    private void Start()
+    {
+        OnStartSound = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        OnStartSound.Play();
+    }
 }
